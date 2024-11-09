073d5840e0179c46713da18cecffa9f55c15a368
Ajonye Paul <ajonyechase@gmail.com>
2024-11-09 23:04:23 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./pages/**/*.{html,js}", "./index.html"],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#7047EB",
+			},
+		},
+	},
+	plugins: [],
+};
